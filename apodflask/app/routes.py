05cdc03fcf327a3f78751a775c29a266d1269f6f
valkar <valkarthecoder@gmail.com>
2021-03-21 11:19:51 -0700

    Added Logic to retrive information from NASA API
    - created basic template to recieve information from api and display it
    - created endpoint /today
    - added index.html which displays date, image, explanation taken from api.

--- apodflask/app/routes.py
+++ apodflask/app/routes.py
@@ -1,6 +1,28 @@
 # used for managing endpoints
+import requests
+import json
+from flask import render_template
+from app import app
 
+API_KEY = 'MzabBeAiCVcA8ZHLnP5a132aNRYMTevN2w3nPH5s'
+    
 @app.route('/')
-@app.route('/index')
-def index():
-    return 'Hello World'
+@app.route('/today')
+def picture_of_day():
+    response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={ API_KEY }')
+    data = response.json()
+    # gets image date from response
+    date = data.get('date')
+    # gets image explanation from response
+    explanation = data.get('explanation')
+    # gets image title
+    image_title = data.get('title')
+    # two urls are included 1 HD and 1 non hd will look for hdurl first
+    # then url unsure if this is that necessary yet but just in case.
+    if data.get('hdurl'):
+        url = data.get('hdurl')
+    else:
+        url = data.get('url')
+
+    return render_template('index.html', title=image_title,date= date, 
+                            explanation=explanation, url=url)
