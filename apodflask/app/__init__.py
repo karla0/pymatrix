48c1edfda72dd6401a6e4249d156a1b13799f841
valkar <valkarthecoder@gmail.com>
2021-03-21 14:57:27 -0700

    Added Fontawesome and some icond to Nav Bar
    - added icons to nav bar links
    - added colors to icons
    - put my fontawesome key cdn in base.html scripts to be able to gain access to icons

--- apodflask/app/__init__.py
+++ apodflask/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_fontawesome import FontAwesome
 
 app = Flask(__name__)
 
 # this is where we define bootstrap enables
 # bootstrap/base.html
 bootstrap = Bootstrap(app)
+fa = FontAwesome(app)
 
 from app import routes
