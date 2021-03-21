7302cadb0575f6b2465a1d9b02f0d5f81766d23c
valkar <valkarthecoder@gmail.com>
2021-03-21 13:13:32 -0700

    Minor Updates
    - Removed uneeded code
    - Updated file system to allow for better oganization

--- apodflask/app/routes.py
+++ apodflask/app/routes.py
@@ -10,13 +10,8 @@ API_KEY = 'MzabBeAiCVcA8ZHLnP5a132aNRYMTevN2w3nPH5s'
 @app.route('/today')
 def picture_of_day():
     response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={ API_KEY }')
+    # data dict
     data = response.json()
-    # gets image date from response
-    date = data.get('date')
-    # gets image explanation from response
-    explanation = data.get('explanation')
-    # gets image title
-    image_title = data.get('title')
     # two urls are included 1 HD and 1 non hd will look for hdurl first
     # then url unsure if this is that necessary yet but just in case.
     if data.get('hdurl'):
@@ -24,5 +19,4 @@ def picture_of_day():
     else:
         url = data.get('url')
 
-    return render_template('index.html', title=image_title,date= date, 
-                            explanation=explanation, url=url)
+    return render_template('today/today.html', image=data, url=url)
